8a9d1a90883d9e2151ac24bf04fc033132965bf7
Lorenzo Sorrells <lsorrells1996@gmail.com>
2022-03-23 16:30:07 -0500

    index and show routes for activities as well as not found error handeling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::API
     include ActionController::Cookies
     rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
     before_action :authorize
 
     def authorize 
@@ -15,5 +16,9 @@ class ApplicationController < ActionController::API
 
     def render_unprocessable_entity(invalid)
         render json: {errors: invalid.record.errors.full_messages}, status: :unprocessable_entity
+    end
+
+    def render_not_found(error)
+        render json: {error: "#{error.model} not found"}, status: :not_found
     end
 end
